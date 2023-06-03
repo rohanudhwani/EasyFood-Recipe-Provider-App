7b489338e493b5662277b024b8b82255a6c33573
Rohan Udhwani <rohanudhwani2@gmail.com>
2023-06-03 17:50:33 +0530

    added youtube link connection

--- app/src/main/java/com/example/easyfood/activities/MealActivity.kt
+++ app/src/main/java/com/example/easyfood/activities/MealActivity.kt
@@ -1,7 +1,10 @@
 package com.example.easyfood.activities
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
@@ -17,6 +20,7 @@ class MealActivity : AppCompatActivity() {
     private lateinit var mealId:String
     private lateinit var mealName:String
     private lateinit var mealThumb:String
+    private lateinit var youtubeLink:String
 
     private lateinit var binding:ActivityMealBinding
 
@@ -32,8 +36,19 @@ class MealActivity : AppCompatActivity() {
         getMealInformationFromIntent()
         setInformationInViews()
 
+        loadingCase()
+
         mealMVVM.getMealDetail(mealId)
         observerMealDetailsLiveData()
+
+        onYoutubeImageClick()
+    }
+
+    private fun onYoutubeImageClick() {
+        binding.imgYoutube.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(youtubeLink))
+            startActivity(intent)
+        }
     }
 
     private fun observerMealDetailsLiveData() {
@@ -44,8 +59,11 @@ class MealActivity : AppCompatActivity() {
                 binding.tvCategory.text = "Category : ${meal!!.strCategory}"
                 binding.tvArea.text = "Area : ${meal!!.strArea}"
                 binding.tvInstructionsSteps.text = meal.strInstructions
+
+                youtubeLink = meal.strYoutube
             }
         })
+        responseCase()
     }
 
     private fun setInformationInViews() {
@@ -66,5 +84,23 @@ class MealActivity : AppCompatActivity() {
         mealName = intent.getStringExtra(HomeFragment.MEAL_NAME)!!
         mealThumb = intent.getStringExtra(HomeFragment.MEAL_THUMB)!!
 
+    }
+
+    private fun loadingCase(){
+        binding.progressBar.visibility = View.VISIBLE
+        binding.btnAddToFavorites.visibility = View.INVISIBLE
+        binding.tvInstructions.visibility = View.INVISIBLE
+        binding.tvCategory.visibility = View.INVISIBLE
+        binding.tvArea.visibility = View.INVISIBLE
+        binding.imgYoutube.visibility = View.INVISIBLE
+    }
+
+    private fun responseCase(){
+        binding.progressBar.visibility = View.INVISIBLE
+        binding.btnAddToFavorites.visibility = View.VISIBLE
+        binding.tvInstructions.visibility = View.VISIBLE
+        binding.tvCategory.visibility = View.VISIBLE
+        binding.tvArea.visibility = View.VISIBLE
+        binding.imgYoutube.visibility = View.VISIBLE
     }
 }
